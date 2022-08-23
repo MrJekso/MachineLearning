90df40a3a5e1537f517bfbecb76d1916fe2fbb87
root <root@localhost.localdomain>
2022-08-23 12:00:15 +0000

    Append new function scan ports and speed test network

--- main.py
+++ main.py
@@ -2,8 +2,28 @@ import platform
 import socket
 import psutil
 import os
+import speedtest
+import socket
+import datetime
+
+def scan_ports(min_port,max_port):
+	ports=[]
+	for port in range(min_port, max_port):
+		sock=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		sock.settimeout(0.5)
+		try:
+			connect = sock.connect(("127.0.0.1",port))
+			ports.append(port)
+			sock.close()
+		except:
+			pass
+	return ports
 
 def info_pc(ip_server):
+	start = datetime.datetime.now()
+
+	st = speedtest.Speedtest()
+
 	s=socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 	s.connect(("8.8.8.8",80))
 
@@ -22,6 +42,15 @@ def info_pc(ip_server):
 	hdd_used=f"{round(hdd.used/(2**30),2)}"
 	hdd_free=f"{round(hdd.free/(2**30),2)}"
 	hdd_proc=F"{round(hdd.percent)}"
+	net_download=round(st.download()/(2**20),2)
+	net_upload=round(st.upload()/(2**20),2)
+	servernames=[]
+	st.get_servers(servernames)
+	net_ping=round(st.results.ping/(2**20),2)
+	ports_list=scan_ports(0,10000)
+	port_str = ""
+	for port in ports_list:
+		port_str+=str(port)+" "
 
 	print(f"[*] OS: {os_name}")
 	print(f"[*] HOSTNAME: {hostname}")
@@ -29,7 +58,9 @@ def info_pc(ip_server):
 	print(f"[*] CPU: {cpu} count:{cpu_count} {cpu_proc}%")
 	print(f"[*] RAM: {ram_total}GB {ram_proc}%")
 	print(f"[*] HDD/SSD: TOTAL={hdd_total}GB USED={hdd_used}GB FREE={hdd_free}GB {hdd_proc}%")
-	print(f"[*] NETWORK: {2+2}")
-	print(f"[*] OPEN PORT: {2+2}")
+	print(f"[*] NETWORK: DOWNLOAD:{net_download}MB UPLAOD:{net_upload}MB PING:{net_upload}MB")
+	print(f"[*] OPEN PORT: {port_str}")
 
+	ends = datetime.datetime.now()
+	print(f"[*] TIME: {ends-start}")
 info_pc("work")
